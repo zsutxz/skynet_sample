336e255fb7b62e5283158f174dcedc0d8bda3209
zsutxz <zsutxz.gmail.com>
2017-06-22 21:23:20 +0800

    commit files

---
+++ service/testsocket.lua
@@ -0,0 +1,54 @@
+local skynet = require "skynet"
+local socket = require "socket"
+
+local mode , id = ...
+
+local function echo(id)
+	socket.start(id)
+	socket.write(id, "Hello Skynet\n")
+	while true do
+		local str = socket.read(id)
+		if str then
+			print(str)
+			socket.write(id, "send socket")
+		else
+			print("clost socket!")
+			socket.close(id)
+			return
+		end
+	end
+end
+
+if mode == "agent" then
+	id = tonumber(id)
+	print("agent:"..id)
+	skynet.start(function()
+		skynet.fork(function()
+			echo(id)
+			skynet.exit()
+		end)
+	end)
+else
+	local function accept(id)
+		socket.start(id)
+		--socket.write(id, "Hello Skynet\n")
+		skynet.newservice(SERVICE_NAME, "agent", id)
+		-- notice: Some data on this connection(id) may lost before new service start.
+		-- So, be careful when you want to use start / abandon / start .
+		socket.abandon(id)
+	end
+
+	skynet.start(function()
+		local id = socket.listen("127.0.0.1", 8880)
+		print("Listen socket :", "127.0.0.1", 8880)
+
+		socket.start(id , function(id, addr)
+			print("connect from " .. addr .. " " .. id)
+			-- you have choices :
+			-- 1. skynet.newservice("testsocket", "agent", id)
+			-- 2. skynet.fork(echo, id)
+			-- 3. accept(id)
+			skynet.newservice("testsocket", "agent", id)
+		end)
+	end)
+end
